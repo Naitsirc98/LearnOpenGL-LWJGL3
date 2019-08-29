63499eb069c2f91b904e20a3f7acdaf2612cec20
Cristian Herrera <naitsirc98@gmail.com>
2019-08-29 17:57:29 +0100

    Working on Textures chapter

--- LearnOpenGL-LWJGL3/src/learnopengl/p1_getting_started/ch41_textures/Textures.java
+++ LearnOpenGL-LWJGL3/src/learnopengl/p1_getting_started/ch41_textures/Textures.java
@@ -1,22 +1,26 @@
-package learnopengl.p1_getting_started.ch33_shader_class;
+package learnopengl.p1_getting_started.ch41_textures;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.stb.STBImage.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.util.logging.Logger;
 
 import org.lwjgl.glfw.GLFWFramebufferSizeCallbackI;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GLCapabilities;
+import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.Platform;
 
 import learnopengl.util.Shader1;
 
-public class ShaderClass {
+public class Textures {
 
 	private static Logger logger = Logger.getAnonymousLogger();
 
@@ -31,13 +35,19 @@ public class ShaderClass {
 	private static final int SRC_WIDTH = 800;
 	private static final int SRC_HEIGHT = 600;
 
-	
+
 	private static final float[] VERTICES = {
-	        // positions         // colors
-	         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-	        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-	         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+			// positions          // colors           // texture coords
+			0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+			0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+			-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+			-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
 	}; 
+
+	private static final int[] INDICES = {
+			0, 1, 3, // first triangle
+			1, 2, 3  // second triangle
+	};
 
 
 	public static void main(String[] args) {
@@ -75,19 +85,18 @@ public class ShaderClass {
 		}
 
 		// Build and compile our shader program
-		final String dir = "src/learnopengl/p1_getting_started/ch33_shader_class/";
-		Shader1 ourShader = new Shader1(dir+"ch33_shader.vs", dir+"ch33_shader.fs"); // You can name your shader files however you like
-		
+		final String dir = "src/learnopengl/p1_getting_started/ch41_textures/";
+		Shader1 ourShader = new Shader1(dir+"ch41_texture.vs", dir+"ch41_texture.fs");
+
 		// Set up vertex data, the Vertex Buffer Object (VBO) and the Vertex Array Object (VAO)
 		final int vao = glGenVertexArrays();
 		final int vbo = glGenBuffers();
-		setUpVertexData(vao, vbo);
-
-	    // Bind the VAO (it was already bound, but just to demonstrate): seeing as we only have a single VAO we can 
-	    // just bind it beforehand before rendering the respective triangle; this is another approach.
-	    glBindVertexArray(vao);
-
-
+		final int ebo = glGenBuffers();
+		setUpVertexData(vao, vbo, ebo);
+		
+		// Load Texture
+		final int texture = loadTexture("resources/textures/container.jpg");
+		
 		// Render loop
 		while(!glfwWindowShouldClose(window)) {
 
@@ -97,11 +106,13 @@ public class ShaderClass {
 			// Clear the screen
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
+			
+			// Bind texture
+			glBindTexture(GL_TEXTURE_2D, texture);
 
-			// Draw triangle
 			ourShader.use();
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-			// glBindVertexArray(0); // No need to unbind it every time
+			glBindVertexArray(vao);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 			// Swap buffers and poll IO events (key/mouse events)
 			glfwSwapBuffers(window);
@@ -112,14 +123,15 @@ public class ShaderClass {
 		// Deallocate all resources when no longer necessary
 		glDeleteVertexArrays(vao);
 		glDeleteBuffers(vbo);
+		glDeleteTextures(texture);
 		ourShader.delete();
 
 		// Clear all allocated resources by GLFW
 		glfwTerminate();
 
 	}
-	
-	private static void setUpVertexData(int vao, int vbo) {
+
+	private static void setUpVertexData(int vao, int vbo, int ebo) {
 		// Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 		glBindVertexArray(vao);
 
@@ -127,19 +139,67 @@ public class ShaderClass {
 		glBufferData(GL_ARRAY_BUFFER, VERTICES, GL_STATIC_DRAW);
 
 		// Position
-		glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.BYTES, NULL);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 8 * Float.BYTES, 0);
 		glEnableVertexAttribArray(0);
-		
+
 		// Color
-		glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
+		glVertexAttribPointer(1, 3, GL_FLOAT, false, 8 * Float.BYTES, 3 * Float.BYTES);
 		glEnableVertexAttribArray(1);
+
+		// Texture coordinates
+		glVertexAttribPointer(2, 2, GL_FLOAT, false, 8 * Float.BYTES, 6 * Float.BYTES);
+		glEnableVertexAttribArray(2);
+
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, INDICES, GL_STATIC_DRAW);
 
 		// Note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 		glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
+		// Remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+		// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 		glBindVertexArray(0); 
+	}
+
+	private static int loadTexture(String path) {
+
+		final int texture = glGenTextures();
+
+		glBindTexture(GL_TEXTURE_2D, texture); // All upcoming GL_TEXTURE_2D operations now have effect on this texture object
+		// Set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // Set texture wrapping to GL_REPEAT (default wrapping method)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// Set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		
+		// Load image, create texture and generate mipmaps
+		try(MemoryStack stack = MemoryStack.stackPush()) {
+			
+			IntBuffer width = stack.ints(0);
+			IntBuffer height = stack.ints(0);
+			IntBuffer nrChannels = stack.ints(0);
+			
+			ByteBuffer data = stbi_load(path, width, height, nrChannels, 0);
+			
+			if(data != null) {
+				
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width.get(0), height.get(0), 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+				glGenerateMipmap(GL_TEXTURE_2D);
+				
+			} else {
+				logger.severe("Failed to load texture: " + path);
+			}
+			
+			stbi_image_free(data);
+			
+		}
+
+		return texture;
 	}
 
 
