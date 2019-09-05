6d8c0dae60ffe4cef21ac8274a785a1858498018
Cristian Herrera <naitsirc98@gmail.com>
2019-09-05 14:02:54 +0100

    Cubemaps chapter complete

---
+++ LearnOpenGL-LWJGL3/src/learnopengl/p4_advanced_opengl/ch06_2_cubemap_environment_mapping/CubemapsEnvironmentMapping.java
@@ -0,0 +1,504 @@
+package learnopengl.p4_advanced_opengl.ch06_2_cubemap_environment_mapping;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.*;
+import static org.lwjgl.opengl.GL13.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.stb.STBImage.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.logging.Logger;
+
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFWCursorPosCallbackI;
+import org.lwjgl.glfw.GLFWFramebufferSizeCallbackI;
+import org.lwjgl.glfw.GLFWScrollCallbackI;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GLCapabilities;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.Platform;
+
+import learnopengl.util.Camera;
+import learnopengl.util.Camera.CameraMovement;
+import learnopengl.util.Shader;
+
+public class CubemapsEnvironmentMapping {
+
+	private static Logger logger = Logger.getAnonymousLogger();
+
+	private static boolean updateProjection = true;
+
+	// Window size
+	private static int windowWidth = 1280;
+	private static int windowHeight = 720;
+
+	// Camera
+	private static Camera camera = new Camera();
+	static {
+		camera.position.z = 3.0f;
+	}
+	private static float lastX = (float)windowWidth / 2.0f;
+	private static float lastY = (float)windowHeight / 2.0f;
+	private static boolean firstMouseInput = true;
+
+	// Timing
+	private static float deltaTime = 0.0f; // Time between current frame and last frame
+	private static float lastFrame = 0.0f;
+
+
+	// Vertex Data
+	private static final float CUBE_VERTICES[] = {
+			// positions          // normals
+			-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+			-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+			-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+			0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+			-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+			-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+			0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+			0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+			0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+			-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f
+	};
+
+	private static final float[] SKYBOX_VERTICES = {
+			// positions          
+			-1.0f,  1.0f, -1.0f,
+			-1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f,  1.0f, -1.0f,
+			-1.0f,  1.0f, -1.0f,
+
+			-1.0f, -1.0f,  1.0f,
+			-1.0f, -1.0f, -1.0f,
+			-1.0f,  1.0f, -1.0f,
+			-1.0f,  1.0f, -1.0f,
+			-1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f,
+
+			1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+
+			-1.0f, -1.0f,  1.0f,
+			-1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f, -1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f,
+
+			-1.0f,  1.0f, -1.0f,
+			1.0f,  1.0f, -1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			-1.0f,  1.0f,  1.0f,
+			-1.0f,  1.0f, -1.0f,
+
+			-1.0f, -1.0f, -1.0f,
+			-1.0f, -1.0f,  1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			-1.0f, -1.0f,  1.0f,
+			1.0f, -1.0f, 1.0f	
+	};
+
+
+	// ============== Callbacks ==============
+
+	private static final GLFWFramebufferSizeCallbackI FRAMEBUFFER_SIZE_CALLBACK = (window, width, height) -> {
+		// make sure the viewport matches the new window dimensions; note that width and 
+		// height will be significantly larger than specified on retina displays.
+		glViewport(0, 0, width, height);
+		// Also update the window width and height variables to correctly set the projection matrix
+		windowWidth = width;
+		windowHeight = height;
+		updateProjection = true;
+	};
+
+	private static final GLFWCursorPosCallbackI MOUSE_MOVE_CALLBACK = (window, xpos, ypos) -> {
+
+		if(firstMouseInput) {
+			lastX = (float)xpos;
+			lastY = (float)ypos;
+			firstMouseInput = false;
+		}
+
+		float xoffset = (float)xpos - lastX;
+		float yoffset = lastY - (float)ypos; // Reversed since y-coordinates go from bottom to top
+		lastX = (float)xpos;
+		lastY = (float)ypos;
+
+		camera.processMouseMovement(xoffset, yoffset, true);
+	};
+
+	private static final GLFWScrollCallbackI SCROLL_CALLBACK = (window, xoffset, yoffset) -> {
+		camera.processMouseScroll((float)yoffset);
+		updateProjection = true;
+	};
+
+
+	public static void main(String[] args) {
+
+		// Initialize GLFW
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		// #ifdef __APPLE__
+		if(Platform.get() == Platform.MACOSX) {
+			glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		}
+
+		// Window creation
+
+		final long window = glfwCreateWindow(windowWidth, windowHeight, "LearnOpenGL", NULL, NULL);
+
+		if(window == NULL) {
+			logger.severe("Failed to create GLFW Window");
+			glfwTerminate();
+			return;
+		}
+
+		glfwMakeContextCurrent(window);
+		glfwSetFramebufferSizeCallback(window, FRAMEBUFFER_SIZE_CALLBACK);
+		glfwSetCursorPosCallback(window, MOUSE_MOVE_CALLBACK);
+		glfwSetScrollCallback(window, SCROLL_CALLBACK);
+
+		// Tell GLFW to capture our mouse
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+		// Load OpenGL functions
+		final GLCapabilities gl = GL.createCapabilities();
+		if(gl == null) {
+			logger.severe("Failed to initialize OpenGL");
+			glfwTerminate();
+			return;
+		}
+
+		// Configure global OpenGL state
+		glEnable(GL_DEPTH_TEST);
+
+		// Build and compile our shader program
+		final String dir = CubemapsEnvironmentMapping.class.getResource(".").getFile();
+		Shader shader = new Shader(dir+"cubemaps.vs", dir+"cubemaps.fs");
+		Shader skyboxShader = new Shader(dir+"skybox.vs", dir+"skybox.fs");
+
+		// Cube
+		final int cubeVAO = glGenVertexArrays();
+		final int cubeVBO = glGenBuffers();
+		setUpVertexData(cubeVAO, cubeVBO, CUBE_VERTICES);
+
+		// Skybox VAO
+		final int skyboxVAO = glGenVertexArrays();
+		final int skyboxVBO = glGenBuffers();
+		glBindVertexArray(skyboxVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+		glBufferData(GL_ARRAY_BUFFER, SKYBOX_VERTICES, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0L);
+
+		// Load textures
+		final int cubeTexture = loadTexture("resources/textures/marble.jpg", false);
+
+		final String[] faces = {
+				"resources/textures/skybox/right.jpg",
+				"resources/textures/skybox/left.jpg",
+				"resources/textures/skybox/top.jpg",
+				"resources/textures/skybox/bottom.jpg",
+				"resources/textures/skybox/front.jpg",
+				"resources/textures/skybox/back.jpg"
+		};
+
+		final int cubemapTexture = loadCubemap(faces);
+
+		// Pass projection matrix to shader (as projection matrix rarely changes there's no need to do this per frame)
+		// ** This is true as long as you don't change the window size!
+		// That's why I check every frame if the projection matrix has to be changed
+		Matrix4f projection = new Matrix4f();
+
+		// Create the model matrix before enter the loop to avoid calling new every frame
+		Matrix4f model = new Matrix4f();
+
+		// Shader configuration
+		shader.use();
+		shader.setInt("skybox",	0);
+		skyboxShader.use();
+		skyboxShader.setInt("skybox", 0);
+		
+		// Since we only use the skybox texture, bind it before render loop
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+
+		// Render loop
+		while(!glfwWindowShouldClose(window)) {
+
+			// Per-frame time logic
+			final float currentFrame = (float)glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+
+			// Input
+			processInput(window);
+
+			// Make sure we clear the framebuffer's content
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			shader.use();
+			// Update projection matrix if necessary
+			if(updateProjection) {
+				projection.setPerspective((float)Math.toRadians(camera.zoom), (float)windowWidth / (float)windowHeight, 
+						0.1f, 100.0f);
+				shader.setMat4("projection", projection);
+				updateProjection = false;
+			}
+
+			// Camera/view transformations
+			final Matrix4f view = camera.getViewMatrix();
+			shader.setMat4("view", view);
+			shader.setVec3("cameraPos", camera.position);
+
+			// Cube
+			glBindVertexArray(cubeVAO);
+			shader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+
+			// Draw skybox at last
+			glDepthFunc(GL_LEQUAL); // Change depth function so depth test passes when values are less or equal to depth buffer's content
+
+			skyboxShader.use();
+			camera.getViewMatrix().get3x3(new Matrix3f()).get(view); // Remove translation from the view matrix
+			skyboxShader.setMat4("view", view);
+			skyboxShader.setMat4("projection", projection);
+			// Skybox cube
+			glBindVertexArray(skyboxVAO);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+			glBindVertexArray(0);
+			glDepthFunc(GL_LESS); // Set depth function back to default
+
+			// Swap buffers and poll IO events (key/mouse events)
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+
+		}
+
+		// Deallocate all resources when no longer necessary
+		glDeleteVertexArrays(cubeVAO);
+		glDeleteVertexArrays(skyboxVAO);
+		glDeleteBuffers(cubeVBO);
+		glDeleteBuffers(skyboxVBO);
+		glDeleteTextures(cubeTexture);
+		shader.delete();
+		skyboxShader.delete();
+
+		// Clear all allocated resources by GLFW
+		glfwTerminate();
+
+	}
+
+	/**
+	 * Loads a cubemap texture from 6 individual texture faces
+	 * Order:
+	 * +X (right)
+	 * -X (left)
+	 * +Y (top)
+	 * -Y (bottom)
+	 * +Z (front) 
+	 * -Z (back) 
+	 * */
+	private static int loadCubemap(String[] faces) {
+
+		final int texture = glGenTextures();
+		glBindTexture(GL_TEXTURE_CUBE_MAP, texture);
+
+		try(MemoryStack stack = MemoryStack.stackPush()) {
+
+			IntBuffer width = stack.ints(0);
+			IntBuffer height = stack.ints(0);
+			IntBuffer nrChannels = stack.ints(0);
+
+			for(int i = 0;i < faces.length;i++) {
+
+				ByteBuffer data = stbi_load(faces[i], width, height, nrChannels, 0);
+
+				if(data != null) {
+
+					final int format = getImageFormat(nrChannels.get(0));
+
+					glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width.get(0), height.get(0),
+							0, format, GL_UNSIGNED_BYTE, data);					
+
+				} else {
+					throw new RuntimeException("Cubmap texture failed to load at path: " + faces[i]);
+				}
+
+			}
+
+		}
+
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+		return texture;
+	}
+
+	private static void setUpVertexData(int vao, int vbo, float[] vertexData) {
+		// Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+		glBindVertexArray(vao);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, vertexData, GL_STATIC_DRAW);
+
+		// Position
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.BYTES, 0);
+		glEnableVertexAttribArray(0);
+
+		// Normal
+		glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
+		glEnableVertexAttribArray(1);
+
+		// Note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+		glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+		glBindVertexArray(0); 
+	}
+
+	private static int loadTexture(String path, boolean flipY) {
+
+		final int texture = glGenTextures();
+
+		glBindTexture(GL_TEXTURE_2D, texture); // All upcoming GL_TEXTURE_2D operations now have effect on this texture object
+		// Set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// Set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// Load image, create texture and generate mipmaps
+		try(MemoryStack stack = MemoryStack.stackPush()) {
+
+			IntBuffer width = stack.ints(0);
+			IntBuffer height = stack.ints(0);
+			IntBuffer nrChannels = stack.ints(0);
+
+			stbi_set_flip_vertically_on_load(flipY); // Tell stb_image.h whether to flip loaded texture's on the y-axis or not.
+
+			ByteBuffer data = stbi_load(path, width, height, nrChannels, 0);
+
+			if(data != null) {
+
+				int format = getImageFormat(nrChannels.get(0));
+
+				glTexImage2D(GL_TEXTURE_2D, 0, format, width.get(0), height.get(0), 0, format, GL_UNSIGNED_BYTE, data);
+				glGenerateMipmap(GL_TEXTURE_2D);
+
+			} else {
+				logger.severe("Failed to load texture: " + path);
+			}
+
+			stbi_image_free(data);
+
+		}
+
+		return texture;
+	}
+
+
+	private static int getImageFormat(int nrChannels) {
+		switch(nrChannels) {
+		case 1:
+			return GL_RED;
+		case 2:
+			return GL_RG;
+		case 3:
+			return GL_RGB;
+		case 4:
+			return GL_RGBA;
+		default:
+			throw new RuntimeException("Unexpected number of channels");
+		}
+	}
+
+	private static void processInput(long window) {
+		// Close window when ESC key is pressed
+		if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, true);
+
+		}
+
+		final float speed = camera.movementSpeed;
+
+		// Bonus: if left shift key is pressed, you double the speed!
+		if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+			camera.movementSpeed *= 2.0f;
+		}
+
+		if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+			camera.processKeyboard(CameraMovement.FORWARD, deltaTime);
+		}
+
+		if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+			camera.processKeyboard(CameraMovement.BACKWARD, deltaTime);
+		}
+
+		if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+			camera.processKeyboard(CameraMovement.LEFT, deltaTime);
+		}
+
+		if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+			camera.processKeyboard(CameraMovement.RIGHT, deltaTime);
+		}
+
+		camera.movementSpeed = speed;
+
+	}
+
+}
