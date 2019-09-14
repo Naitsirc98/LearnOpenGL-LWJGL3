3bc248ee89f4c008daefd850f39f5b5d76f829ff
Cristian Herrera <naitsirc98@gmail.com>
2019-09-14 16:42:43 +0100

    HDR chapter complete

---
+++ LearnOpenGL-LWJGL3/src/learnopengl/p5_advanced_lighting/ch06_hdr/HDR.java
@@ -0,0 +1,512 @@
+package learnopengl.p5_advanced_lighting.ch06_hdr;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.stb.STBImage.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.logging.Logger;
+
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.lwjgl.glfw.GLFWCursorPosCallbackI;
+import org.lwjgl.glfw.GLFWFramebufferSizeCallbackI;
+import org.lwjgl.glfw.GLFWScrollCallbackI;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GLCapabilities;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.Platform;
+
+import learnopengl.util.Camera;
+import learnopengl.util.Camera.CameraMovement;
+import learnopengl.util.Shader;
+
+public class HDR {
+
+	private static Logger logger = Logger.getAnonymousLogger();
+
+	private static boolean updateProjection = true;
+
+	// Window size
+	private static int windowWidth = 1280;
+	private static int windowHeight = 720;
+
+	// Camera
+	private static Camera camera = new Camera();
+	static {
+		camera.position.z = -1.5f;
+	}
+	private static float lastX = (float)windowWidth / 2.0f;
+	private static float lastY = (float)windowHeight / 2.0f;
+	private static boolean firstMouseInput = true;
+
+	// Timing
+	private static float deltaTime = 0.0f; // Time between current frame and last frame
+	private static float lastFrame = 0.0f;
+
+	// HDR
+	private static boolean hdr = true;
+	private static boolean hdrKeyPressed = false;
+	private static float exposure = 1.0f;
+
+
+	// Vertex Data
+	private static final float[] CUBE_VERTICES = {
+			// back face
+			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+			1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+			1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+			1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+			-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+			// front face
+			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+			1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+			1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+			1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+			-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+			// left face
+			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+			-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+			-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+			// right face
+			1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+			1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+			1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
+			1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+			1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+			1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
+			// bottom face
+			-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+			1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+			1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+			1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+			-1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+			-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+			// top face
+			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+			1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+			1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
+			1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+			-1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f // bottom-left        
+	};
+
+	private static final float[] QUAD_VERTICES = {
+			// positions        // texture Coords
+			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+			1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+			1.0f, -1.0f, 0.0f, 1.0f, 0.0f,	
+	};
+
+
+	// ============== Callbacks ==============
+
+	private static final GLFWFramebufferSizeCallbackI FRAMEBUFFER_SIZE_CALLBACK = (window, width, height) -> {
+		// make sure the viewport matches the new window dimensions; note that width and 
+		// height will be significantly larger than specified on retina displays.
+		glViewport(0, 0, width, height);
+		// Also update the window width and height variables to correctly set the projection matrix
+		windowWidth = width;
+		windowHeight = height;
+		updateProjection = true;
+	};
+
+	private static final GLFWCursorPosCallbackI MOUSE_MOVE_CALLBACK = (window, xpos, ypos) -> {
+
+		if(firstMouseInput) {
+			lastX = (float)xpos;
+			lastY = (float)ypos;
+			firstMouseInput = false;
+		}
+
+		float xoffset = (float)xpos - lastX;
+		float yoffset = lastY - (float)ypos; // Reversed since y-coordinates go from bottom to top
+		lastX = (float)xpos;
+		lastY = (float)ypos;
+
+		camera.processMouseMovement(xoffset, yoffset, true);
+	};
+
+	private static final GLFWScrollCallbackI SCROLL_CALLBACK = (window, xoffset, yoffset) -> {
+		camera.processMouseScroll((float)yoffset);
+		updateProjection = true;
+	};
+
+
+	public static void main(String[] args) {
+
+		// Initialize GLFW
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		// #ifdef __APPLE__
+		if(Platform.get() == Platform.MACOSX) {
+			glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		}
+
+		// Window creation
+
+		final long window = glfwCreateWindow(windowWidth, windowHeight, "LearnOpenGL", NULL, NULL);
+
+		if(window == NULL) {
+			logger.severe("Failed to create GLFW Window");
+			glfwTerminate();
+			return;
+		}
+
+		glfwMakeContextCurrent(window);
+		glfwSetFramebufferSizeCallback(window, FRAMEBUFFER_SIZE_CALLBACK);
+		glfwSetCursorPosCallback(window, MOUSE_MOVE_CALLBACK);
+		glfwSetScrollCallback(window, SCROLL_CALLBACK);
+
+		// Tell GLFW to capture our mouse
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+		// Load OpenGL functions
+		final GLCapabilities gl = GL.createCapabilities();
+		if(gl == null) {
+			logger.severe("Failed to initialize OpenGL");
+			glfwTerminate();
+			return;
+		}
+
+		// Configure global OpenGL state
+		glEnable(GL_DEPTH_TEST);
+
+		// Build and compile our shader program
+		final String dir = HDR.class.getResource(".").getFile();
+		Shader shader = new Shader(dir+"lighting.vs", dir+"lighting.fs");
+		Shader hdrShader = new Shader(dir+"hdr.vs", dir+"hdr.fs");
+
+		// Cube
+		final int cubeVAO = glGenVertexArrays();
+		final int cubeVBO = glGenBuffers();
+		setUpVertexData(cubeVAO, cubeVBO, CUBE_VERTICES);
+
+		// Quad
+		final int quadVAO = glGenVertexArrays();
+		final int quadVBO = glGenBuffers();
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, QUAD_VERTICES, GL_STATIC_DRAW);
+		// Position
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 5 * Float.BYTES, 0L);
+		// Texture coords
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, false, 5 * Float.BYTES, 3 * Float.BYTES);
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
+		// Load textures
+		final int woodTexture = loadTexture("resources/textures/wood.png", false);
+
+		// Configure floating point framebuffer
+		final int hdrFBO = glGenFramebuffers();
+		// Create depth texture
+		final int colorBuffer = glGenTextures();
+		glBindTexture(GL_TEXTURE_2D, colorBuffer);
+		nglTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, windowWidth, windowHeight, 0, GL_RGBA, GL_FLOAT, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// Create depth buffer (renderbuffer)
+		final int rboDepth = glGenRenderbuffers();
+		glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, windowWidth, windowHeight);
+		// Attach buffers
+		glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer, 0);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+
+		if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+			throw new RuntimeException("Framebuffer is not complete");
+		}
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+		// Lighting info
+		// Positions
+		final Vector3f[] lightPositions = {
+				new Vector3f( 0.0f,  0.0f, 49.5f), // Back light
+				new Vector3f(-1.4f, -1.9f, 9.0f), 
+				new Vector3f( 0.0f, -1.8f, 4.0f), 
+				new Vector3f( 0.8f, -1.7f, 6.0f), 
+		};
+
+		// Colors
+		final Vector3f[] lightColors = {
+				new Vector3f(200.0f, 200.0f, 200.0f),
+				new Vector3f(0.1f, 0.0f, 0.0f), 
+				new Vector3f(0.0f, 0.0f, 0.2f), 
+				new Vector3f(0.0f, 0.1f, 0.0f), 
+		};
+
+		// Shader configuration
+		shader.use();
+		shader.setInt("diffuseTexture", 0);
+		// Set lighting uniforms
+		for(int i = 0;i < lightPositions.length;i++) {
+			shader.setVec3("lights["+i+"].Position", lightPositions[i]);
+			shader.setVec3("lights["+i+"].Color", lightColors[i]);
+		}
+
+		hdrShader.use();
+		hdrShader.setInt("hdrBuffer", 0);
+
+		// Pass projection matrix to shader (as projection matrix rarely changes there's no need to do this per frame)
+		// ** This is true as long as you don't change the window size!
+		// That's why I check every frame if the projection matrix has to be changed
+		Matrix4f projection = new Matrix4f();
+
+		// Create the model matrix before enter the loop to avoid calling new every frame
+		Matrix4f model = new Matrix4f();
+
+
+		// Render loop
+		while(!glfwWindowShouldClose(window)) {
+
+			// Per-frame time logic
+			final float currentFrame = (float)glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+
+			// Input
+			processInput(window);
+
+			if(updateProjection) {
+				projection.setPerspective((float)Math.toRadians(camera.zoom), (float)windowWidth / (float)windowHeight, 
+						0.1f, 100.0f);
+				updateProjection = false;
+			}
+			Matrix4f view = camera.getViewMatrix();
+
+			// Change light position over time
+			// lightPos.x = (float)Math.sin(glfwGetTime()) * 3.0f;
+			// lightPos.y = 5.0f + (float)Math.cos(glfwGetTime()) * 1.0f;
+			// lightPos.z = (float)Math.cos(glfwGetTime()) * 2.0f;
+
+			// Clear screen
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			// 1. Render scene into floating point framebuffer
+			glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			shader.use();
+			shader.setMat4("projection", projection);
+			shader.setMat4("view", view);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, woodTexture);
+			shader.setVec3("viewPos", camera.position);
+			// Render tunnel
+			model.translation(0.0f, 0.0f, 25.0f);
+			model.scale(2.5f, 2.5f, 27.5f);
+			shader.setMat4("model", model);
+			shader.setBool("inverse_normals", true);
+			renderCube(cubeVAO);
+
+			// 2. Now render floating point color buffer to 2D quad and tonemap HDR colors to default framebuffer's
+			// (clamped) color range
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			hdrShader.use();
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, colorBuffer);
+			hdrShader.setBool("hdr", hdr);
+			hdrShader.setFloat("exposure", exposure);
+			renderQuad(quadVAO);
+
+			System.out.println("hdr: " + (hdr ? "on" : "off") + "| exposure: " + exposure);
+
+			// Swap buffers and poll IO events (key/mouse events)
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+
+		}
+
+		// Deallocate all resources when no longer necessary
+		glDeleteVertexArrays(cubeVAO);
+		glDeleteVertexArrays(quadVAO);
+		glDeleteBuffers(cubeVBO);
+		glDeleteBuffers(quadVAO);
+		glDeleteTextures(woodTexture);
+		glDeleteTextures(colorBuffer);
+		shader.delete();
+		hdrShader.delete();
+		glDeleteFramebuffers(hdrFBO);
+
+		// Clear all allocated resources by GLFW
+		glfwTerminate();
+
+	}
+
+	private static void renderQuad(int quadVAO) {
+		glBindVertexArray(quadVAO);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindVertexArray(0);
+	}
+
+	private static void renderCube(int cubeVAO) {
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+	}
+
+	private static void setUpVertexData(int vao, int vbo, float[] vertexData) {
+		// Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+		glBindVertexArray(vao);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, vertexData, GL_STATIC_DRAW);
+
+		// Position
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 8 * Float.BYTES, 0L);
+		glEnableVertexAttribArray(0);
+		// Normal
+		glVertexAttribPointer(1, 3, GL_FLOAT, false, 8 * Float.BYTES, 3 * Float.BYTES);
+		glEnableVertexAttribArray(1);
+		// Texture coords
+		glVertexAttribPointer(2, 2, GL_FLOAT, false, 8 * Float.BYTES, 6 * Float.BYTES);
+		glEnableVertexAttribArray(2);
+
+		// Note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+		glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+		glBindVertexArray(0); 
+	}
+
+	private static int loadTexture(String path, boolean gammaCorrection) {
+
+		final int texture = glGenTextures();
+
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		// Load image, create texture and generate mipmaps
+		try(MemoryStack stack = MemoryStack.stackPush()) {
+
+			IntBuffer width = stack.ints(0);
+			IntBuffer height = stack.ints(0);
+			IntBuffer nrChannels = stack.ints(0);
+
+			ByteBuffer data = stbi_load(path, width, height, nrChannels, 0);
+
+			if(data != null) {
+
+				int internalFormat = 0;
+				int dataFormat = 0;
+
+				switch(nrChannels.get(0)) {
+
+				case 1:
+					internalFormat = dataFormat = GL_RED;
+					break;
+				case 2:
+					internalFormat = dataFormat = GL_RG;
+					break;
+				case 3:
+					dataFormat = gammaCorrection ? GL_SRGB : GL_RGB;
+					internalFormat = GL_RGB;
+					break;
+				case 4:
+					dataFormat = gammaCorrection ? GL_SRGB_ALPHA : GL_RGBA;
+					internalFormat = GL_RGBA;
+					break;
+				default:
+					logger.severe("Unexpected number of channels");
+				}
+				
+		        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width.get(0), height.get(0), 0, dataFormat, GL_UNSIGNED_BYTE, data);
+		        glGenerateMipmap(GL_TEXTURE_2D);
+
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			} else {
+				logger.severe("Failed to load texture: " + path);
+			}
+
+			stbi_image_free(data);
+
+		}
+
+		return texture;
+	}
+
+
+	private static void processInput(long window) {
+		// Close window when ESC key is pressed
+		if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, true);
+
+		}
+
+		final float speed = camera.movementSpeed;
+
+		// Bonus: if left shift key is pressed, you double the speed!
+		if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+			camera.movementSpeed *= 2.0f;
+		}
+
+		if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+			camera.processKeyboard(CameraMovement.FORWARD, deltaTime);
+		}
+
+		if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+			camera.processKeyboard(CameraMovement.BACKWARD, deltaTime);
+		}
+
+		if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+			camera.processKeyboard(CameraMovement.LEFT, deltaTime);
+		}
+
+		if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+			camera.processKeyboard(CameraMovement.RIGHT, deltaTime);
+		}
+		
+		if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && !hdrKeyPressed) {
+			hdr = !hdr;
+			hdrKeyPressed = true;
+			
+		}else if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE) {
+			hdrKeyPressed = false;
+		}
+		
+		if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+			if(exposure > 0.0f) {
+				// exposure -= 0.001f;
+				exposure -= 0.01f;
+			} else {
+				exposure = 0.0f;
+			}
+			
+		} else if(glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+			// exposure += 0.001f;
+			exposure += 0.01f;
+		}
+
+		camera.movementSpeed = speed;
+
+	}
+
+}
